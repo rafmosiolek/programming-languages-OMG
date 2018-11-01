9c0eb7db089cad79c702880101740c98f8670000
RafM <rkjmosiolek@gmail.com>
2018-11-01 14:52:45 +0000

    add ADD method

--- language.js
+++ language.js
@@ -1,17 +1,41 @@
 var myProgram = [
     ['#print', 'hello, world!'],
-    ['#print', 'this is my programming language!']
+    ['#print', '2 + 2 + 2 =', ['#add', ['#add', 2, 2], 2]]
 ]
+
+function evalExpression(expression) {
+    if (Array.isArray(expression)) {
+        return evalFunctionCall(expression)
+    } else {
+        return expression;
+    }
+}
+
+function evalFunctionCall(expression) {
+    var [command, ...rawArgs] = expression;
+    var args = [];
+
+    for (rawArg of rawArgs) {
+        var evaluatedArg = evalExpression(rawArg);
+        args.push(evaluatedArg);
+    }
+
+    switch (command) {
+        case '#print':
+            console.log(...args);
+            break;
+        case '#add':
+            return args[0] + args[1];
+            break;
+        default:
+            throw new Error('Unknown command ' + command);
+    }
+}
+
 
 function run(expressions) {
     for (var expression of expressions) {
-        var [command, ...args] = expression;
-
-        if (command === '#print') {
-            console.log(...args);
-        } else {
-            throw new Error('Unknown command ' + command);
-        }
+        evalFunctionCall(expression);
     }
 }
 
